5bea37897988d2158269492d0ae10c0964e631ea
“Tea <teateearu@gmail.com>
2017-11-27 00:48:35 +0100

    G

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -15,20 +15,27 @@ class Codebreaker
 
     def guess(input)
       if input.length != 4
-        output.puts "Try guessing a number with four digits"
-        return
+        return output.puts "Try guessing a number with four digits"
       end
 
       result = ''
+      @guesses_array = []
 
       for i in 0..3
-        if input[i] == @secret_number[i]
+        game_number = @secret_number[i]
+        player_guess = input [i]
+        if player_guess == game_number
           result += '+'
-        elsif @secret_number.include? input[i]
+          @guesses_array.push(player_guess)
+        elsif not_already_matched(game_number, player_guess)
           result += '-'
+          @guesses_array.push(player_guess)
         end
       end
       output.puts result.split('').sort.join
+    end
+    def not_already_matched(game_number, player_guess)
+      @secret_number.include? player_guess unless @guesses_array.include? player_guess
     end
   end
 end
